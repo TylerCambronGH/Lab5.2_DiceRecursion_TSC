8ea39a18d90cd5f5a82ac3c16c517de0032a6a1b
TylerCambronGH <77807623+TylerCambronGH@users.noreply.github.com>
2021-09-30 14:38:36 -0400

    Update RecDieRollerDriver.java

--- RecDieRollerDriver.java
+++ RecDieRollerDriver.java
@@ -2,6 +2,10 @@
 public class RecDieRollerDriver {
 	public static void main(String[] args) {
 		RecDiceRoller dice = new RecDiceRoller();
-		dice.roll();
+		for (int i=0; i<1000; i++) {
+			dice.roll();
+		}
+		System.out.println("There was " + dice.getTotalRolls() + " total rolls for 1000 2's.");
+		System.out.println("There was a mean of " + (dice.getTotalRolls()/1000.0) + " to get a 2.");
 	}
 }
